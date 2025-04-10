6790b1881582414a5e11d97b92ad1d840d6b10f5
christophe <christophe.delory@labom2iformation.fr>
2025-04-10 09:42:47 +0200

    correction exerice stock et api todo

---
+++ demos/10_node/exos/correction/classes/fileService.js
@@ -0,0 +1,59 @@
+import { Todo } from "./todo.js"
+import {writeFileSync, readFileSync} from "fs"
+export class FileService {
+    constructor() {
+        this.todos = []
+        this.count = 0
+        this.fichier = "data.json"
+    }
+
+    read() {
+        const contenu = readFileSync(this.fichier).toString()
+        this.todos = JSON.parse(contenu)
+        this.count = (this.todos[this.todos.length - 1] != undefined) ? this.todos[this.todos.length - 1].id : 0
+    }
+
+    write() {
+        writeFileSync(this.fichier, JSON.stringify(this.todos))
+    }
+
+    //Méthode de création
+    createTodo(title, content) {
+        const todo = new Todo(++this.count, title, content)
+        this.todos.push(todo)
+        this.write()
+    }
+
+    //Méthode pour récupérer un todo par id
+    findTodoById(id) {
+        return this.todos.find(t => t.id == id)
+    }
+
+    //Methode pour rechercher todos par titre
+    searchTodosByTitle(search) {
+        return this.todos.filter(t => t.title.includes(search))
+    }
+
+
+    //Methode pour mettre à jour le statut d'un todo
+    updateTodoStatus(id) {
+        const todo = this.findTodoById(id)
+        if(todo != undefined) {
+            todo.status = !todo.status
+            this.write()
+            return true
+        }
+        return false
+    }
+
+    //Methode pour supprimer un todo
+    deleteTodo(id) {
+        const todo = this.findTodoById(id)
+        if(todo != undefined) {
+            this.todos = this.todos.filter(t => t.id != id)
+            this.write()
+            return true
+        }
+        return false
+    }
+}
---
+++ demos/10_node/exos/correction/index.js
@@ -0,0 +1,64 @@
+import express from "express"
+import { FileService } from "./classes/fileService.js"
+import cors from "cors"
+//Un objet pour gérer les données
+const dataService = new FileService()
+
+//Notre objet express
+const api = express()
+
+//Utilisation d'un middleware json
+api.use(express.json())
+
+api.use(cors({origin:"*"}))
+//Endpoint pour créer un todo
+api.post('/todos', (req, res) => {
+    const {title, content} = req.body
+    if(title != undefined && content != undefined) {
+        dataService.createTodo(title, content)
+        res.json({message: "todo ajouté", error:false})
+    }
+    else
+        res.json({message: "Merci de envoyer un titre et un contenu"})
+})
+
+//Endpoint pour récuperer la liste des todos
+api.get('/todos', (req, res) => {
+    res.json(dataService.todos)
+})
+
+//Endpoint pour récupérer un seul todo
+api.get('/todos/:id', (req, res) => {
+    const todo = dataService.findTodoById(req.params.id)
+    if(todo != undefined)
+        res.json(todo)
+    else
+        res.json({mesage: "aucun todo avec cet id"})
+})
+
+//Endpoint pour la recherche
+api.get('/todos/search/:search', (req, res) => {
+    res.json(dataService.searchTodosByTitle(req.params.search))
+})
+
+//EndPoint pour la suppression d'un todo
+api.delete('/todos/:id', (req, res) => {
+    if(dataService.deleteTodo(req.params.id)) 
+        res.json({message: 'todo supprimé'})
+    else 
+        res.json({message: 'erreur suppression todo'})
+})
+
+//EndPoint pour la mise à jour du statut
+api.patch('/todos/:id', (req, res) => {
+    if(dataService.updateTodoStatus(req.params.id)) {
+        res.json({message : "statut modifié"})
+    }
+    else {
+        res.json({message: "erreur de modification du statut"})
+    }
+})
+
+api.listen(80, () => {
+    dataService.read()
+})
